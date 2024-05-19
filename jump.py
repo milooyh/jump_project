4d96b78252b519dea4a4ba0f3e9f0de9f93cbe5a
milooyh <hyunyoo5064@naver.com>
2024-05-19 20:51:07 +0900

    코드 설명 주석 추가

--- jump.py
+++ jump.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 
 # 화면 크기 설정
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600 #가로 세로
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("점프 점프")
 
@@ -10,6 +10,7 @@ pygame.display.set_caption("점프 점프")
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+BLACK = (0,0,0)
 FLOOR_COLOR = (144, 228, 144)
 
 # 캐릭터 속성 설정
@@ -29,7 +30,7 @@ platform_color = BLUE
 
 # 장애물 속성 설정
 obstacle_width, obstacle_height = 80, 30
-obstacle_color = RED
+obstacle_color = BLACK
 
 # 블록 좌표 설정 
 blocks_positions = [
@@ -53,11 +54,11 @@ class Block:
         self.x = x
         self.y = y
 
-# 블록 리스트 초기화
+# 블록, 장애물 리스트 초기화
 blocks = [Block(x, y) for x, y in blocks_positions]
 obstacles = [Block(x, y) for x, y in obstacles_positions]
 
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() # 게임 프레임 속도 제어
 
 # 충돌 감지
 def check_collision(character, blocks):
@@ -68,14 +69,15 @@ def check_collision(character, blocks):
 
 # 게임 루프
 running = True
-vertical_momentum = 0
-is_on_ground = True
-space_pressed = False
+vertical_momentum = 0 # 수직 이동 속도
+is_on_ground = True # 바닥에 있는지 여부
+space_pressed = False # 스페이스키 눌림 여부
 
 while running:
     screen.fill(WHITE)
     character_rect = pygame.Rect(character_x, character_y, character_width, character_height)
 
+    # 게임 정료 이벤트, 스페이스 키 눌림 이벤트 처리
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -86,21 +88,24 @@ while running:
             if event.key == pygame.K_SPACE:
                 space_pressed = False
 
+    # 스페이스 눌리고 바닥에 있으면 점프
     if space_pressed and is_on_ground:
         vertical_momentum = -jump_speed
         is_on_ground = False
 
+    # 상하좌우 이동키
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         character_x -= character_speed
     if keys[pygame.K_RIGHT]:
         character_x += character_speed
 
-    character_x = max(0, min(SCREEN_WIDTH - character_width, character_x))
+    character_x = max(0, min(SCREEN_WIDTH - character_width, character_x)) # 화면 밖으로 나가지 않게게
     vertical_momentum += gravity
     character_y += vertical_momentum
     character_y = min(character_y, floor_y - character_height)
 
+    # 바닥
     pygame.draw.rect(screen, FLOOR_COLOR, (0, floor_y, SCREEN_WIDTH, floor_height))
 
     # 장애물 이동 및 그리기
@@ -116,10 +121,12 @@ while running:
             character_y = max(block_collided.y, obstacle_collided.y) - character_height
             vertical_momentum = 0
             is_on_ground = True
-
+            
+    # 블록 처리
     for block in blocks:
         pygame.draw.rect(screen, platform_color, (block.x, block.y, platform_width, platform_height))
 
+    # 캐릭터 처리
     pygame.draw.rect(screen, RED, character_rect)
 
     pygame.display.update()
