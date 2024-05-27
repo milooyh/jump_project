9e4678fd9c20a038f291cba39f533ebd84b1ef00
milooyh <hyunyoo5064@naver.com>
2024-05-27 16:22:43 +0900

    설명 주석 추가

--- screen.py
+++ screen.py
@@ -1,9 +1,11 @@
 import pygame
+import sys
 from setting import *
 from obstacle import *
 from block import *
 from portal import *
 
+# 시작 화면 보여주기
 def show_start_screen(screen):
     screen.fill(WHITE)
     font = pygame.font.Font(None, 64)
@@ -12,6 +14,7 @@ def show_start_screen(screen):
     screen.blit(text, text_rect)
     pygame.display.update()
     wait_for_space()
+
 
 def wait_for_space():
     waiting = True
@@ -23,6 +26,7 @@ def wait_for_space():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 waiting = False
 
+# 클리어 화면
 def show_clear_screen(screen):
     screen.fill(WHITE)
     font = pygame.font.Font(None, 64)
@@ -32,6 +36,7 @@ def show_clear_screen(screen):
     pygame.display.update()
     pygame.time.wait(2000)
 
+# 게임 오버 화면
 def show_game_over_screen(screen):
     screen.fill(WHITE)
     font = pygame.font.Font(None, 64)
@@ -41,6 +46,7 @@ def show_game_over_screen(screen):
     pygame.display.update()
     pygame.time.wait(2000)
 
+# 요소 그리기 
 def draw_game_elements(screen, character_rect):
     pygame.draw.rect(screen, RED, character_rect)
     for block in blocks:
