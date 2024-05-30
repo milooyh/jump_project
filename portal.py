2b0c492b63e6c0612891c58a0236093ddd2c99fe
milooyh <hyunyoo5064@naver.com>
2024-05-30 20:42:47 +0900

    포털 이미지 적용

--- portal.py
+++ portal.py
@@ -1,5 +1,3 @@
-# portal.py
-
 import pygame
 from setting import *
 
@@ -10,7 +8,8 @@ class Portal:
         self.width = 50
         self.height = 50
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-
+        self.image = pygame.image.load('portal.png').convert_alpha()  # 이미지 불러오기
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))  # 이미지 크기 조절
+    
     def draw(self, screen):
-        pygame.draw.rect(screen, (0, 255, 0), self.rect)
-
+        screen.blit(self.image, (self.x, self.y))  # 이미지 그리기
